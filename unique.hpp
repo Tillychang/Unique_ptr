335d669793b0bd17da933bad86bcda9fbd42fbef
Ubuntu12.04 <test@ubuntu.(none)>
2014-10-11 14:35:56 +0800

    增加reset，swap 功能

--- unique.hpp
+++ unique.hpp
@@ -1,6 +1,10 @@
 #ifndef UNIQUE_HPP_
 #define UNIQUE_HPP_ 
 
+#include <algorithm>
+#include <stddef.h>
+#include <utility>
+
 template <typename T>
 class UniquePtr
 {
@@ -10,9 +14,25 @@ class UniquePtr
         typedef T &reference ;
 
         explicit UniquePtr(T *ptr = NULL) throw() ;
-        UniquePtr(UniquePtr &&) throw() ;
-        UniquePtr &operator=(UniquePtr &&) throw() ;
-        ~UniquePtr() ;
+        UniquePtr(UniquePtr &&other) throw() 
+            :ptr_(other.ptr_)
+        {
+            other.ptr_ = NULL ;   //让传入的临时变量的指针指向 NULL 避免bug。
+        }
+        UniquePtr &operator=(UniquePtr &&other) throw() 
+        {
+            if(this != &other)
+            {
+                delete ptr_ ;
+                ptr_ = other.ptr_ ;
+                other.ptr_ = NULL ;
+            }
+            return *this ;
+        }
+        ~UniquePtr() 
+        {
+            delete ptr_ ; 
+        }
 
         reference operator*() const throw() 
         {
@@ -26,11 +46,35 @@ class UniquePtr
         
         pointer get() const throw() 
         {
-            
+           return ptr_ ;
         }
 
+        pointer relese() 
+        {
+            pointer temp(ptr_) ;
+            ptr_ = NULL ;
+            return temp ;
+        }
+
+        void reset(T *ptr = NULL) 
+        {
+            if(ptr != ptr_)
+            {
+                delete ptr_ ;
+                ptr_ = ptr;
+            }
+        }
+
+        swap(UniquePtr &other)
+        {
+            std::swap(ptr_, other.ptr_) ;
+        }
+        
 
     private:
+
+        void operator=(const UniquePtr &) ;
+        UniquePtr(const UniquePtr &) ;
         pointer ptr_ ;
 }
 
